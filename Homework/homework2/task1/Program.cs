c1d5eba21dbbd90797c81e69b131efe2ef7fbe5b
Darya <Pokhilenko.darya@bk.ru>
2022-11-22 19:31:15 +0700

    homework2

---
+++ Homework/homework2/task1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+void Twonum (int num)
+{
+    int des = num / 10;
+    int des2 =des % 10;
+    Console.WriteLine($"Twonumber {des2}");
+}
+Console.Write("Input ThreeDigitNumber");
+Twonum(Convert.ToInt32(Console.ReadLine()));
