6685dc5aec5ae38bff03b21d8cca45dfca1f8985
Darya <Pokhilenko.darya@bk.ru>
2022-11-23 19:02:01 +0700

    homework 23.11.22

---
+++ Homework/homework3/task1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да*/
+
+
+Console.Write("Input number ");
+string num = Console.ReadLine();
+void number (string num)
+{
+    if (num.Length == 5)
+{
+    number(num);
+}
+else
+{
+    Console.WriteLine($"Input correct number");
+}
+
+    if (num[0]==num[4] || num[1]==num[3])
+    {
+      Console.WriteLine($"Your number is palindrome");
+    }
+    else
+{
+     Console.WriteLine($"Your number is not palindrome");
+}
+
+}
---
+++ Homework/homework3/task3/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+
+Console.Write("Input number");
+int number=Convert.ToInt32(Console.ReadLine());
+int cube = Convert.ToInt32(Console.ReadLine());
+int size =0;
+if (number >= size);
+{
+   while (cube[0]== number*number*number);
+    size++;
+}
+Застряла  здесь.не понимаю, какие значения нужно присвоить,чтобы заработало.
